911c6855f5491811a798ca3f5f5beab2de62aba3
isfetkos <sfetkosilias@gmail.com>
2023-02-11 14:39:32 +0100

    login

--- src/components/login-component.vue
+++ src/components/login-component.vue
@@ -1,32 +1,36 @@
 <template>
-    <v-parallax :src="parallaxSrc">
-        <p id="h3-text">Admin Login</p>
-        <component :is="siteComponent"></component>
-        <div class="v_card">
-              <v-text-field
-              class="v_text"
-                label="Username"
-                type="password"
-              ></v-text-field>
-              <v-text-field
-              class="v_text"
-                label="Password"
-                type="password"
-              ></v-text-field>
-            <v-btn class="admin-btn" variant="tonal"
-            >Login</v-btn>
-            <p id="h4-text">{{ $t("t_easter") }}</p>
-        </div>
+  <v-parallax :src="parallaxSrc">
+    <p id="h3-text">Admin Login</p>
+    <component :is="siteComponent"></component>
+    <div class="login-container">
+    <div class="v_card">
+      <v-text-field
+        class="v_text"
+        label="Username"
+        type="name"
+      ></v-text-field>
+      <v-text-field
+        class="v_text"
+        label="Password"
+        type="password"
+      ></v-text-field>
+      <div class="login-container2">
+      <v-btn class="admin-btn" variant="tonal">Login</v-btn>
+    </div>
+      <p id="h4-text">{{ $t("t_easter") }}</p>
+    
+  </div>
+  </div>
   </v-parallax>
 </template>
 <script>
-export default { 
-    name: "login-component", 
-    data() {
-        return {
-            parallaxSrc:"images/bitmap.svg"
-        }
-    }
+export default {
+  name: "login-component",
+  data() {
+    return {
+      parallaxSrc: "images/bitmap.svg",
     };
+  },
+};
 </script>
 <style scoped></style>
