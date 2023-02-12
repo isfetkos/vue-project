082a1fd5c7d5ab8a2c934644f824150cbe411c2f
isfetkos <sfetkosilias@gmail.com>
2023-02-12 01:44:03 +0100

    bugs

--- src/components/blog-admin-component.vue
+++ src/components/blog-admin-component.vue
@@ -2,8 +2,10 @@
     <v-parallax :src="parallaxSrc">
             <p id="h3-text">{{ $t("t_adminblog_header") }}</p>
             <div class="admin-container">
+            <v-textarea rows="1" label="Title"></v-textarea>
             <v-textarea auto-grow label="Label"></v-textarea>
             <v-file-input label="File input"></v-file-input>
+            <v-btn class="admin-btn" variant="tonal">Submit</v-btn>
             </div>
       </v-parallax>
 </template>
--- src/components/blog-component.vue
+++ src/components/blog-component.vue
@@ -1,6 +1,37 @@
 <template>
     <v-parallax :src="parallaxSrc">
             <p id="h3-text">{{ $t("t_blog_header") }}</p>
+            <transition-group tag="ul" class="blog__feed" name="preview">
+                <li v-for="post in feed" :class="classes" :key="post.id">
+                  <router-link :to="`/read/${post.id}`">
+                    <figure class="preview__figure">
+                      <img :src="post.image"/>
+            
+                      <transition name="fade">
+                        <figcaption v-if="!reading" class="preview__title">
+                            {{ post.title }}
+                        </figcaption>
+                      </transition>
+                    </figure>
+                  </router-link>
+            
+                  <transition name="fade">
+                    <aside v-if="!reading" class="preview__details">
+                      <h5 class="preview__meta">
+                        <router-link class="preview__author"
+                          :to="`/by/${ kebabify(post.author) }`"
+                          @click.native="scrollTo(0)">
+                          {{ post.author }}
+                        </router-link>
+            
+                        <time class="preview__published">
+                            {{ prettyDate(post.published) }}
+                        </time>
+                      </h5>
+                    </aside>
+                  </transition>
+                </li>
+              </transition-group>
       </v-parallax>
 </template>
 <script>
