57ec989de14214a8f4fc7f9cfba3552da574586d
isfetkos <sfetkosilias@gmail.com>
2023-02-18 21:54:12 +0100

    auth0

--- src/components/lessons-component.vue
+++ src/components/lessons-component.vue
@@ -1,13 +1,12 @@
 <template>
-    <v-parallax :src="parallaxSrc">
-        <p id="h3-text">{{ $t("t_lesson_header") }}</p>
-      <div class="children">
+  <v-parallax :src="parallaxSrc">
+    <p id="h3-text">{{ $t("t_lesson_header") }}</p>
+    <div class="children">
       <div class="lesson-part">
         <span class="lesson-circle">
-          <v-icon
-          class="icon-btn lessons-icons"
-          color="var(--clr-lpink)"
-        >mdi-rocket-launch</v-icon>
+          <v-icon class="icon-btn lessons-icons" color="var(--clr-lpink)"
+            >mdi-rocket-launch</v-icon
+          >
         </span>
         <h1 class="lesson-title">{{ $t("t_lesson_header1") }}</h1>
         <hr />
@@ -16,10 +15,9 @@
       </div>
       <div class="lesson-part">
         <span class="lesson-circle">
-          <v-icon
-          class="icon-btn lessons-icons"
-          color="var(--clr-lpink)"
-        >mdi-book-open-page-variant</v-icon>
+          <v-icon class="icon-btn lessons-icons" color="var(--clr-lpink)"
+            >mdi-book-open-page-variant</v-icon
+          >
         </span>
         <h1 class="lesson-title">{{ $t("t_lesson_header2") }}</h1>
         <hr />
@@ -28,27 +26,54 @@
       </div>
       <div class="lesson-part">
         <span class="lesson-circle">
-          <v-icon
-          class="icon-btn lessons-icons"
-          color="var(--clr-lpink)"
-        >mdi-school</v-icon>
-      </span>
+          <v-icon class="icon-btn lessons-icons" color="var(--clr-lpink)"
+            >mdi-school</v-icon
+          >
+        </span>
         <h1 class="lesson-title">{{ $t("t_lesson_header3") }}</h1>
         <hr />
         <p>{{ $t("t_lesson_text3") }}</p>
         <a href="#" class="product__btn btn">Μάθε περισσοτερα</a>
       </div>
     </div>
-
+    <p id="h4-text-white">Για τους νεαρούς μου μαθητές</p>
+    <v-carousel
+      cycle
+      height="400"
+      hide-delimiter-background
+      show-arrows="hover"
+    >
+      <v-carousel-item>
+        <v-sheet color="rgba(0,0,0,0)" height="100%">
+          <div class="d-flex fill-height justify-center align-center">
+            <div class="text-h5"> Η γλώσσα είναι ένα εργαλείο </div>
+          </div>
+        </v-sheet>
+      </v-carousel-item>
+      <v-carousel-item>
+        <v-sheet color="rgba(0,0,0,0)" height="100%">
+          <div class="d-flex fill-height justify-center align-center">
+            <div class="text-h5"> Slide</div>
+          </div>
+        </v-sheet>
+      </v-carousel-item>
+      <v-carousel-item>
+        <v-sheet color="rgba(0,0,0,0)" height="100%">
+          <div class="d-flex fill-height justify-center align-center">
+            <div class="text-h5"> Slide</div>
+          </div>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
   </v-parallax>
 </template>
 <script>
-export default { 
-    data() {
-        return {
-            parallaxSrc:"images/bitmap.svg"
-        }
-    }
+export default {
+  data() {
+    return {
+      parallaxSrc: "images/bitmap.svg",
     };
+  },
+};
 </script>
 <style scoped></style>
--- src/components/login-component.vue
+++ src/components/login-component.vue
@@ -15,10 +15,13 @@
       ></v-text-field>
       <div class="login-container2">
       <router-link to="/blogAdmin">    
-      <v-btn @click="login" class="admin-btn" variant="tonal">Login</v-btn>
+      <!-- <v-btn @click="login" class="admin-btn" variant="tonal">Login</v-btn> -->
+      <div>
+        <button @click="login">Log in</button>
+      </div>
     </router-link>
     </div>
-      <p id="h4-text">{{ $t("t_easter") }}</p>
+      <p id="h5-text">{{ $t("t_easter") }}</p>
   </div>
   </div>
   </v-parallax>
@@ -28,13 +31,13 @@ import { useAuth0 } from '@auth0/auth0-vue';
 export default {
   name: "login-component",
 setup() {
-      // const { loginWithRedirect } = useAuth0();
+      const { loginWithRedirect } = useAuth0();
 
       return {
         parallaxSrc: "images/bitmap.svg",
-        // login: () => {
-        //   loginWithRedirect();
-        // }
+        login: () => {
+        loginWithRedirect();
+         }
       };
     }
   };
